1d4aed101af58e8d4d439bd941c1a0fb6da49d2f
Shiva Marya <maryashiva07@gmail.com>
2026-06-28 02:10:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //javascript file
+// Write your code below:
+
+let fruit = document.querySelectorAll("li");
+
+fruit[4].style.color = "red";
+
+fruit.forEach((fr) => {
+    fr.style.fontStyle = "italic";
+})
